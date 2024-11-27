4c646a193d7d545d10b60a4cb340d228c8cfdb59
Pedro Pazello <pedro.pazello@enjoei.com.br>
2024-11-27 08:19:45 -0300

    add broke code

--- get_ip.js
+++ get_ip.js
@@ -1,8 +1,14 @@
 async function getIP(IPAdress, callback) {
-    const url = `https://ipapi.co/${IPAdress}/json/`;
-    const response = await fetch(url);
-    const responseJSON = await response.json();
-    return callback(responseJSON)
+    const url = `https://ipapi.co/${IPAdress}/json/`
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+
+    if (responseJSON.error == true) {
+        console.log(`error: ${responseJSON.reason}`)
+        return
+    } else {
+        return callback(responseJSON)
+    }
 }
 
-module.exports = getIP;
+module.exports = getIP
---
+++ main.js
@@ -0,0 +1,19 @@
+const getIP = require('./get_ip');
+
+getIP("8.8.8.8", (resp) => {
+    if (resp.ip != undefined) {
+        console.log(`IP: ${resp.ip}`)
+    }
+    
+    if (resp.country_capital != undefined) {
+        console.log(`Country Capital: ${resp.country_capital}`)
+    }
+
+    if (resp.country_code != undefined) {
+        console.log(`Country Code: ${resp.country_code}`)
+    }
+
+    if (resp.timezone != undefined) {
+        console.log(`Country Timezone: ${resp.timezone}`)
+    }
+})
